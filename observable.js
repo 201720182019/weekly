78c04ea445884b66cfeadf1944ff020e867b477b
x <wanghongxin@z.local>
2018-05-19 03:20:35 +0800

    type

--- observable.js
+++ observable.js
@@ -1,4 +1,11 @@
-const data$ = (observer) => {
+interface Observer {
+    <T>(value: T): void;
+}
+interface Observable {
+    <T>(output: Observer<T>): void;
+}
+
+const data$: Observable<number> = (observer: observer<number>) => {
     observer(1);
     observer(2);
     setTimeout(() => {
